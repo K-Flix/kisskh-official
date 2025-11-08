e78f5828f0faad96fce72595456f39c647d80777
Dara Yoeurn <darayoeurn@gmail.com>
2025-11-08 15:15:10 +0000

    Make a clear Navbar background

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -36,7 +36,7 @@ export function Header() {
   const [isSheetOpen, setSheetOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b border-white/10 bg-background/80 backdrop-blur-sm">
+    <header className="sticky top-0 z-40 w-full">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-6 md:gap-10 items-center">
           <Link href="/" className="flex items-center space-x-2">
