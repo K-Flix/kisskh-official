03aebc63a6cdc2e26d5ecce134507b75f187109e
Dara Yoeurn <darayoeurn@gmail.com>
2025-11-08 17:22:45 +0000

    See All show from New to Old

--- src/lib/endpoints.ts
+++ src/lib/endpoints.ts
@@ -6,7 +6,7 @@ export const endpoints: { key: string; title: string; url: string; sort_by?:stri
   
     // TV Shows
     { key: 'trending_tv', title: 'Trending TV Shows', url: `/trending/tv/week?language=en-US`, type: 'tv' },
-    { key: 'k_drama', title: 'K-Dramas', url: `/discover/tv?with_origin_country=KR&language=en-US`, sort_by: 'popularity.desc', type: 'tv' },
+    { key: 'k_drama', title: 'K-Dramas', url: `/discover/tv?with_origin_country=KR&language=en-US`, sort_by: 'first_air_date.desc', type: 'tv' },
     { key: 'c_drama', title: 'C-Dramas', url: `/discover/tv?with_origin_country=CN&language=en-US`, sort_by: 'popularity.desc', type: 'tv' },
     { key: 'anime', title: 'Anime', url: `/discover/tv?with_genres=16&language=en-US`, sort_by: 'popularity.desc', type: 'tv' },
     { key: 'on_the_air_tv', title: 'On The Air TV Shows', url: `/tv/on_the_air?language=en-US`, type: 'tv' },
@@ -16,7 +16,7 @@ export const endpoints: { key: string; title: string; url: string; sort_by?:stri
     { key: 'trending_movies', title: 'Trending Movies', url: `/trending/movie/week?language=en-US`, type: 'movie' },
     { key: 'popular_movies', title: 'Popular Movies', url: `/movie/popular?language=en-US`, type: 'movie' },
     { key: 'now_playing_movies', title: 'Now Playing Movies', url: `/movie/now_playing?language=en-US`, type: 'movie' },
-    { key: 'upcoming_movies', title: 'Upcoming Movies', url: `/movie/upcoming?language=en-US`, type: 'movie' },
+    { key: 'upcoming_movies', title: 'Upcoming Movies', url: `/movie/upcoming?language-en-US`, type: 'movie' },
     { key: 'top_rated_movies', title: 'Top Rated Movies', url: `/movie/top_rated?language=en-US`, type: 'movie' },
   ];
   
