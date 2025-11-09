9c31ea0734db11e000fc6987c679b1debfa8ab42
Dara Yoeurn <darayoeurn@gmail.com>
2025-11-09 06:51:47 +0000

    Popular C-Dramas change to C-Drama

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
       <div className="container py-8 space-y-12">
         <MovieCarousel title="Trending Today" movies={trending} seeAllHref="/discover?category=trending_today&title=Trending Today" />
         <MovieCarousel title="K-Drama" movies={kDramas} seeAllHref="/discover?category=k_drama&title=K-Dramas" />
-        <MovieCarousel title="Popular C-Dramas" movies={cDramas} seeAllHref="/discover?category=c_drama&title=C-Dramas" />
+        <MovieCarousel title="C-Drama" movies={cDramas} seeAllHref="/discover?category=c_drama&title=C-Dramas" />
         <MovieCarousel title="Anime" movies={anime} seeAllHref="/discover?category=anime&title=Anime" />
         <MovieCarousel title="Popular Movies" movies={popularMovies} seeAllHref="/discover?category=popular_movies&title=Popular Movies" />
         <MovieCarousel title="Top Rated" movies={topRated} seeAllHref="/discover?category=top_rated_movies&title=Top Rated Movies" />
