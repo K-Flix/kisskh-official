54efb70608f3d96a142d793417ee674a9f2a3764
Dara Yoeurn <darayoeurn@gmail.com>
2025-11-10 04:23:10 +0000

    Try fixing this error: `Runtime Error: Error: A <Select.Item /> must hav

--- src/components/discover-filters.tsx
+++ src/components/discover-filters.tsx
@@ -46,34 +46,34 @@ export function DiscoverFilters({ genres, countries, years, onFilterChange, onRe
                     <SelectItem value="tv">TV Shows</SelectItem>
                 </SelectContent>
             </Select>
-            <Select value={currentFilters['with_genres'] || ''} onValueChange={(value) => onFilterChange('with_genres', value)}>
+            <Select value={currentFilters['with_genres'] || 'all'} onValueChange={(value) => onFilterChange('with_genres', value)}>
                 <SelectTrigger className="w-full bg-background border-0 focus:ring-2 focus:ring-primary">
                     <SelectValue placeholder="Genre" />
                 </SelectTrigger>
                 <SelectContent>
-                    <SelectItem value="">All Genres</SelectItem>
+                    <SelectItem value="all">All Genres</SelectItem>
                     {genres.map((genre) => (
                         <SelectItem key={genre.id} value={genre.id.toString()}>{genre.name}</SelectItem>
                     ))}
                 </SelectContent>
             </Select>
-            <Select value={currentFilters['with_origin_country'] || ''} onValueChange={(value) => onFilterChange('with_origin_country', value)}>
+            <Select value={currentFilters['with_origin_country'] || 'all'} onValueChange={(value) => onFilterChange('with_origin_country', value)}>
                 <SelectTrigger className="w-full bg-background border-0 focus:ring-2 focus:ring-primary">
                     <SelectValue placeholder="Country" />
                 </SelectTrigger>
                 <SelectContent>
-                     <SelectItem value="">All Countries</SelectItem>
+                     <SelectItem value="all">All Countries</SelectItem>
                     {countries.map((country) => (
                         <SelectItem key={country.iso_3166_1} value={country.iso_3166_1}>{country.english_name}</SelectItem>
                     ))}
                 </SelectContent>
             </Select>
-            <Select value={currentFilters['primary_release_year'] || ''} onValueChange={(value) => onFilterChange('primary_release_year', value)}>
+            <Select value={currentFilters['primary_release_year'] || 'all'} onValueChange={(value) => onFilterChange('primary_release_year', value)}>
                 <SelectTrigger className="w-full bg-background border-0 focus:ring-2 focus:ring-primary">
                     <SelectValue placeholder="Year" />
                 </SelectTrigger>
                 <SelectContent>
-                    <SelectItem value="">All Years</SelectItem>
+                    <SelectItem value="all">All Years</SelectItem>
                     {years.map((year) => (
                         <SelectItem key={year} value={year}>{year}</SelectItem>
                     ))}
