07db6fa0c72a9e983d6c7f1e84bb1636fd136356
Dara Yoeurn <darayoeurn@gmail.com>
2025-11-12 10:55:25 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/theme-wrapper.tsx
+++ src/components/theme-wrapper.tsx
@@ -3,11 +3,21 @@
 
 import { useTheme } from '@/context/theme-context';
 import { Inter } from 'next/font/google';
+import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export function ThemeWrapper({ children }: { children: React.ReactNode }) {
     const { theme } = useTheme();
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return <>{children}</>;
+    }
     
     return (
         <html 
