1a5dec9c5b5783830c38cd9630b03ec73fc98f50
Dara Yoeurn <darayoeurn@gmail.com>
2025-11-14 05:52:44 +0000

    export const FacebookIcon: React.FC<React.SVGProps<SVGSVGElement>> = (pr

--- src/components/social-icons.tsx
+++ src/components/social-icons.tsx
@@ -3,15 +3,8 @@ import { SVGProps } from 'react';
 
 export function FacebookIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      stroke="currentColor"
-      strokeWidth="0"
-    >
-      <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm3 8h-1.5c-.2 0-.5.1-.5.5V10h2l-.2 2h-1.8v5h-3v-5H9V10h1.5V7.5c0-1.4 1.1-2.5 2.5-2.5H16v3z"/>
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" />
     </svg>
   );
 }
